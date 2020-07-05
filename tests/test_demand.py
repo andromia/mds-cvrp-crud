3c26e501bf77986b527c986fd13f1012e4b02d51
Rohan <rohannawathe@hotmail.com>
2020-07-05 20:22:39 +0530

    added extra tests and docstrings

--- tests/test_demand.py
+++ tests/test_demand.py
@@ -2,7 +2,11 @@ import pytest
 import logging
 import json
 
+import string
 import random
+from csv import DictReader
+
+from typing import List
 
 # from flask import FlaskClient
 from werkzeug.wrappers import Response
@@ -11,13 +15,14 @@ from werkzeug.wrappers import Response
 class TestDemand:
     @pytest.fixture(autouse=True)
     def set_demand_endpoint(self, api_base_url):
+        """Set demand endpoint as object variable"""
+
         self.demand_endpoint: str = api_base_url + "/demand"
         logging.debug(f'Demand Endpoint "{self.demand_endpoint}"')
 
     @pytest.fixture(scope="class")
     def sample_demands(self):
         """Return rows of sample demands from csv file"""
-        from csv import DictReader
 
         # Loading data from sample csv
         with open("tests/vrp_testing_data.csv") as sample_demand_file:
@@ -36,15 +41,105 @@ class TestDemand:
 
         return sample_demand_rows
 
-    def test_non_json_request(self, client):
+    @pytest.fixture()
+    def random_demand(self):
+        """Return random generated demand"""
+
+        return {
+            "latitude": random.uniform(-90, 90),
+            "longitude": random.uniform(-180, 180),
+            "quantity": random.uniform(0, 20000),
+            "cluster_id": random.randint(0, 100),
+            "unit": "".join(
+                random.choices(string.ascii_lowercase, k=random.randint(1, 10))
+            ),
+        }
+
+    @pytest.mark.parametrize(
+        "content_type",
+        [
+            "audio/aac",
+            "application/x-abiword",
+            "application/x-freearc",
+            "video/x-msvideo",
+            "application/vnd.amazon.ebook",
+            "application/octet-stream",
+            "image/bmp",
+            "application/x-bzip",
+            "application/x-bzip2",
+            "application/x-csh",
+            "text/css",
+            "text/csv",
+            "application/msword",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+            "application/vnd.ms-fontobject",
+            "application/epub+zip",
+            "application/gzip",
+            "image/gif",
+            "text/html",
+            "image/vnd.microsoft.icon",
+            "text/calendar",
+            "application/java-archive",
+            "image/jpeg",
+            "text/javascript, per the following specifications:",
+            "audio/midi",
+            "text/javascript",
+            "audio/mpeg",
+            "video/mpeg",
+            "application/vnd.apple.installer+xml",
+            "application/vnd.oasis.opendocument.presentation",
+            "application/vnd.oasis.opendocument.spreadsheet",
+            "application/vnd.oasis.opendocument.text",
+            "audio/ogg",
+            "video/ogg",
+            "application/ogg",
+            "audio/opus",
+            "font/otf",
+            "image/png",
+            "application/pdf",
+            "application/x-httpd-php",
+            "application/vnd.ms-powerpoint",
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+            "application/vnd.rar",
+            "application/rtf",
+            "application/x-sh",
+            "image/svg+xml",
+            "application/x-shockwave-flash",
+            "application/x-tar",
+            "image/tiff",
+            "video/mp2t",
+            "font/ttf",
+            "text/plain",
+            "application/vnd.visio",
+            "audio/wav",
+            "audio/webm",
+            "video/webm",
+            "image/webp",
+            "font/woff",
+            "font/woff2",
+            "application/xhtml+xml",
+            "application/vnd.ms-excel",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "application/xml ",
+            "application/vnd.mozilla.xul+xml",
+            "application/zip",
+            "video/3gpp",
+            "video/3gpp2",
+            "application/x-7z-compressed",
+        ],
+    )
+    def test_non_json_request(self, client, content_type):
         """Test with content types other than 'application/json'"""
 
-        logging.info("Testing with content-types other than JSON")
+        logging.info(f"Testing with content-type : {content_type}")
+
         res: Response = client.post(
-            self.demand_endpoint, headers={"Content-Type": "application/xml"}, data=""
+            self.demand_endpoint, headers={"Content-Type": content_type}, data=""
         )
+
         logging.debug(f"Response : {res}")
         logging.debug(f"Response Data : {res.data}")
+
         assert res.status_code == 400
         assert res.headers["Content-Type"] == "application/json"
         assert (
@@ -53,29 +148,42 @@ class TestDemand:
 
     def test_invalid_json(self, client):
         """Test with invalid JSON in request"""
+
         logging.info("Testing with invalid JSON")
         logging.debug(f'Sending request to "{self.demand_endpoint}"')
+
         res: Response = client.post(
             self.demand_endpoint,
             headers={"Content-Type": "application/json"},
-            data='{"dfd',
+            data="".join(
+                random.choices(
+                    string.ascii_letters + "".join(["{", "}", '"', "'"]),
+                    k=random.randint(1, 27),
+                )
+            ),
         )
+
         logging.debug(f"Response : {res}")
         logging.debug(f"Response Data : {res.data}")
+
         assert res.status_code == 400
         assert res.headers["Content-Type"] == "application/json"
         assert res.json["message"] == "Invalid JSON received! Request data must be JSON"
 
     def test_empty_demand(self, client):
         """Test by sending empty demand array"""
+
         logging.info("Testing with empty demands array")
+
         res: Response = client.post(
             self.demand_endpoint,
             headers={"Content-Type": "application/json"},
             json={"demands": []},
         )
+
         logging.debug(f"Response : {res}")
         logging.debug(f"Response Data : {res.data}")
+
         assert res.status_code == 400
         assert res.headers["Content-Type"] == "application/json"
 
@@ -83,26 +191,119 @@ class TestDemand:
         assert error_message == "'demands' is empty"
 
     @pytest.mark.parametrize(
-        "param", ["latitude", "longitude", "cluster_id", "unit_name", "quantity"]
+        "param", ["latitude", "longitude", "unit", "cluster_id", "quantity"]
     )
-    def test_invalid_demand(self, client, param):
+    def test_invalid_demand(self, client, param, random_demand):
         """Test with invalid parameters in demand"""
-        pass
 
-    def test_single_insert(self, client, sample_demands: list):
+        demand = random_demand
+        logging.debug(f"Demand : {demand}")
+
+        invalid_choices = {
+            "latitude": [
+                random.uniform(-10000, -90.1),
+                random.uniform(90.1, 10000),
+                "".join(
+                    random.choices(
+                        string.ascii_letters + "".join(["{", "}", '"', "'"]),
+                        k=random.randint(1, 27),
+                    )
+                ),
+            ],
+            "longitude": [
+                random.uniform(-10000, -90.1),
+                random.uniform(90.1, 10000),
+                "".join(
+                    random.choices(
+                        string.ascii_letters + "".join(["{", "}", '"', "'"]),
+                        k=random.randint(1, 27),
+                    )
+                ),
+            ],
+            "cluster_id": [
+                random.uniform(-10000, 0),
+                random.uniform(0, -10000),
+                random.randint(-10000, 0),
+                random.randint(0, 10000),
+                "".join(
+                    random.choices(
+                        string.ascii_letters + "".join(["{", "}", '"', "'"]),
+                        k=random.randint(1, 27),
+                    )
+                ),
+            ],
+            "unit": [
+                random.uniform(-10000, 0),
+                random.uniform(0, -10000),
+                random.randint(-10000, 0),
+                random.randint(0, 10000),
+                "".join(
+                    random.choices(
+                        string.ascii_letters + "".join(["{", "}", '"', "'"]),
+                        k=random.randint(1, 10),
+                    )
+                ),
+            ],
+            "quantity": [
+                random.randint(-10000, 0),
+                "".join(
+                    random.choices(
+                        string.ascii_letters + "".join(["{", "}", '"', "'"]),
+                        k=random.randint(1, 10),
+                    )
+                ),
+            ],
+        }
+
+        demand[param] = random.choice(invalid_choices[param])
+
+        logging.debug(f"Invalid Demand : {demand}")
+
+        res: Response = client.post(
+            self.demand_endpoint,
+            headers={"Content-Type": "application/json"},
+            json={"demands": [demand]},
+        )
+
+        assert res.status_code == 400
+        assert f"Invalid {param}" in res.json["message"]
+
+    def test_single_insert(self, client, sample_demands: List[dict]):
         """Test with single demand"""
+
         sample_demand = random.choice(sample_demands)
+
         logging.debug(f"Demand : {sample_demand}")
+
         res: Response = client.post(
             self.demand_endpoint,
             headers={"Content-Type": "application/json"},
             json={"demands": [sample_demand]},
         )
+
         logging.debug(f"Response : {res}")
         logging.debug(f"Response Data : {res.data}")
+
         assert res.status_code == 200
         assert res.headers["Content-Type"] == "application/json"
 
-    def test_batch_insert(self, sample_demands: list):
+    def test_batch_insert(self, client, sample_demands: List[dict]):
         """Test with multiple demands"""
-        pass
+
+        logging.info("Testing with multiple demands in one request")
+
+        res: Response = client.post(
+            self.demand_endpoint,
+            headers={"Content-Type": "application/json"},
+            json={"demands": sample_demands},
+        )
+
+        assert res.status_code == 200
+        assert len(res.json) == len(sample_demands)
+
+        for demand, response in zip(sample_demands, res.json):
+            id = response.pop("id")
+            assert isinstance(id, int)
+            assert demand == response
+            response["id"] = id
+
