89ec6060d4af7725140b1075dac2fae6b5a560ea
Rohan <rohannawathe@hotmail.com>
2020-07-09 17:55:38 +0530

    added PUT handler for individual origin

--- app/api/v0_1/origin.py
+++ app/api/v0_1/origin.py
@@ -2,9 +2,12 @@ from . import bp, errors
 
 from flask import request, jsonify, make_response
 
+import logging
+from typing import Dict, Union
+
 from app import db
 
-from app.models import Origin
+from app.models import Origin, Unit
 
 
 @bp.route("/origin", methods=["GET", "POST"])
@@ -21,22 +24,26 @@ def origins():
                 "Incorrect request format! Request data must be JSON"
             )
 
-        data = request.get_json()
+        data = request.get_json(silent=True)
+        if not data:
+            raise errors.InvalidUsage(
+                "Invalid JSON received! Request data must be JSON"
+            )
 
-        if "origin" in data:
-            origin = data["origin"]
+        if "origins" in data:
+            origins = data["origins"]
         else:
-            raise errors.InvalidUsage("'origin' missing in request data")
+            raise errors.InvalidUsage("'origins' missing in request data")
 
-        if not isinstance(origin, list):
-            raise errors.InvalidUsage("'origin' should be list")
+        if not isinstance(origins, list):
+            raise errors.InvalidUsage("'origins' should be list")
 
-        if not origin:
-            raise errors.InvalidUsage("'origin' is empty")
-        elif len(origin) != 1:
-            raise errors.InvalidUsage("'origin' contains more than one object")
+        if not origins:
+            raise errors.InvalidUsage("'origins' is empty")
+        elif len(origins) != 1:
+            raise errors.InvalidUsage("'origins' contains more than one object")
 
-        origin = origin[0]
+        origin = origins[0]
 
         # Checking if origin is valid
         check_origin(origin)
@@ -56,7 +63,48 @@ def origins():
 
         origin["id"] = new_origin.id
 
-        return make_response(jsonify({"origin": origin}), 201)
+        return make_response(jsonify({"origins": [origin]}), 201)
+
+
+@bp.route("/origin/<int:id>", methods=["GET", "PUT"])
+def origin(id):
+    if request.method == "GET":
+        return Origin.query.get_or_404(id)
+    if request.method == "PUT":
+
+        origin: Origin = Origin.query.get_or_404(id)
+
+        if not request.is_json:
+            raise errors.InvalidUsage(
+                "Incorrect request format! Request data must be JSON"
+            )
+
+        data: Union[dict, None] = request.get_json(silent=True)
+        if not data:
+            raise errors.InvalidUsage(
+                "Invalid JSON received! Request data must be JSON"
+            )
+
+        params = ["latitude", "longitude"]
+
+        new_origin: Dict[str, any] = {}
+
+        for param in params:
+            if param in data:
+                new_origin[param] = data[param]
+            else:
+                raise errors.InvalidUsage(f"{param} missing in request data")
+
+        # Validate new data
+        check_origin(new_origin)
+
+        # Update values in DB
+        origin.latitude = new_origin["latitude"]
+        origin.longitude = new_origin["longitude"]
+
+        db.session.commit()
+
+        return make_response(jsonify(origin.to_dict(), 200))
 
 
 def check_origin(origin):
@@ -78,12 +126,6 @@ def check_origin(origin):
 
     if origin["longitude"] < -180 or 180 < origin["longitude"]:
         raise errors.InvalidUsage("Invalid longitude", invalid_object=origin)
-
-
-@bp.route("/origin/<int:id>", methods=["GET"])
-def origin(id):
-    if request.method == "GET":
-        return Origin.query.get_or_404(id)
 
 
 def is_float(x: any):
